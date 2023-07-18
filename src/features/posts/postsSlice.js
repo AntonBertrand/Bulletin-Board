4dcfa2e6245c5d539e879a6d989e39911162fb1c
Anton <56392640+AntonBertrand@users.noreply.github.com>
2023-07-18 16:17:22 +0100

    Added Time Ago and Reaction Features
    Posts will now display how long ago the post was created and users can click on reactions on posts

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,16 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { nanoid } from "@reduxjs/toolkit";
+import { sub } from 'date-fns';
 
 const initialState = [
     {
         id: '1',
         title: 'Learning Redux Toolkit',
         content: "I've heard good things.",
+        date: sub(new Date(), {minutes: 10}).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
     },
     {
         id: '2',
         title: 'Slices...',
         content: "The more I say slice, the more I want pizza.",
+        date: sub(new Date(), {minutes: 5}).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
     }
 ]
 
@@ -28,9 +45,24 @@ const postsSlice = createSlice({
                         id: nanoid(),
                         title, 
                         content,
-                        userId
+                        userId,
+                        date: new Date().toISOString(),
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
+                        }
                     }
                 }
+            }
+        },
+        reactionAdded(state, action) {
+            const { postId, reaction } = action.payload
+            const existingPost = state.find(post => post.id === postId)
+            if (existingPost) {
+                existingPost.reactions[reaction]++
             }
         }
     }
@@ -38,6 +70,6 @@ const postsSlice = createSlice({
 
 export const selectAllPosts = (state) => state.posts;
 
-export const { postAdded } = postsSlice.actions
+export const { postAdded, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
